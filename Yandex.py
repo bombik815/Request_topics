a4c8ec15496f819242561c66a328ece272d83084
bombik815 <58844959+bombik815@users.noreply.github.com>
2021-06-30 18:41:14 +0300

    Add files via upload

---
+++ Yandex.py
@@ -0,0 +1,46 @@
+import requests
+from pprint import pprint
+
+
+# читаем токен
+with open('token_yandex', 'r') as file_object:
+    token = file_object.read().strip()
+
+class YaDisk:
+    def __init__(self, token):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+
+    def get_f_list(self):
+        file_url = 'https://cloud-api.yandex.net/v1/disk/resources/files'
+        headers = self.get_headers()
+        response = requests.get(file_url, headers=headers)
+        return response.json()
+
+    def _get_upload_link(self, disk_file_path):
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = self.get_headers()
+        params = {"path": disk_file_path, "overwrite": "True"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        pprint(response.json())
+        return response.json()
+
+    def upload_file_to_disk(self, disk_file_path, filename):
+        href = self._get_upload_link(disk_file_path=disk_file_path).get("href", "")
+        response = requests.put(href, data=open(filename, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print("OK")
+
+
+#
+#pprint(ya.get_f_list())
+
+if __name__ == '__main__':
+    ya = YaDisk(token=token)
+    ya.upload_file_to_disk(disk_file_path='TEMP/test_file.txt', filename='file.txt')
